f4a689e8056b9600de24f3287d7824ea6a4190fe
Jeremy Novak <jgnovak@gmail.com>
2014-07-12 13:49:42 -0600

    Using Swift version of SKTUtils. Added score keeping. Minor cleanup.

---
+++ swifty/Classes/GameSounds.swift
@@ -0,0 +1,13 @@
+//
+//  GameSounds.swift
+//  swifty
+//
+//  Created by Jeremy Novak on 7/12/14.
+//  Copyright (c) 2014 Jeremy Novak. All rights reserved.
+//
+
+import UIKit
+
+class GameSounds: NSObject {
+   
+}
